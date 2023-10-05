9abd1eb2d4b46b6bfbb3bfd623f59f98eb352c0d
Sergiyan92 <sergiyan@ukr.net>
2023-10-05 15:52:23 +0300

    answer the question

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,65 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(enteredKey: Key): void;
+
+  comeIn(person: Person): void {
+    if (
+      this.door &&
+      person.getKey().getSignature() === this.key.getSignature()
+    ) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} entered the house.`);
+    } else {
+      console.log("Access denied.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(enteredKey: Key): void {
+    if (enteredKey.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("Invalid key. The door remains closed.");
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+house.openDoor(person.getKey()); // The door is open.
+house.comeIn(person); // person entered the house.
